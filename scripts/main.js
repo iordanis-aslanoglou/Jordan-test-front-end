b56028ecad711968d5cafad23a2442213773518b
iordanis-aslanoglou <iordanis.aslanoglou@isobar.com>
2018-02-08 12:40:44 +0200

    jordan_tests2

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+$(".open").on("click", function(){
+    $(".overlay, .modal").addClass("active");
+  });
+  
+  $(".close, .overlay").on("click", function(){
+     $(".overlay, .modal").removeClass("active");
+  });
+  
+  $(document).keyup(function(e) {
+    if (e.keyCode === 27) {
+      $(".overlay, .modal").removeClass("active");
+    }
+  });
